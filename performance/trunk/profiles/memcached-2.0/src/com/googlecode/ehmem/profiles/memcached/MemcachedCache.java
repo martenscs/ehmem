76695261343c8a44d6edae716a9a84dbca48dcec
abashev <abashev@78729e4b-5a45-0410-8d40-d9a0f5577ee2>
2008-02-11 13:25:23 +0000

    Added memcached configuration in xml file

--- performance/trunk/profiles/memcached-2.0/src/com/googlecode/ehmem/profiles/memcached/MemcachedCache.java
+++ performance/trunk/profiles/memcached-2.0/src/com/googlecode/ehmem/profiles/memcached/MemcachedCache.java
@@ -19,52 +19,6 @@ public class MemcachedCache extends CacheAdapter {
     private MemCachedClient client = new MemCachedClient();
     private Statistics statistics = new Statistics();
 
-    private static final SockIOPool pool;
-
-    // set up connection pool once at class load
-    static {
-        // server list and weights
-        String[] servers = {
-                "127.0.0.1:11212",
-                "192.168.0.1:11211",
-                "127.0.0.1:11211"
-        };
-
-        Integer[] weights = {1, 1, 1};
-
-        // grab an instance of our connection pool
-        pool = SockIOPool.getInstance();
-
-        // set the servers and the weights
-        pool.setServers( servers );
-        pool.setWeights( weights );
-
-        // set some basic pool settings
-        // 5 initial, 5 min, and 250 max conns
-        // and set the max idle time for a conn
-        // to 6 hours
-        pool.setInitConn( 5 );
-        pool.setMinConn( 5 );
-        pool.setMaxConn( 250 );
-        pool.setMaxIdle( 1000 * 60 * 60 * 6 );
-
-        // set the sleep for the maint thread
-        // it will wake up every x seconds and
-        // maintain the pool size
-        pool.setMaintSleep( 30 );
-
-        // set some TCP settings
-        // disable nagle
-        // set the read timeout to 3 secs
-        // and don't set a connect timeout
-        pool.setNagle( false );
-        pool.setSocketTO( 3000 );
-        pool.setSocketConnectTO( 0 );
-
-        // initialize the connection pool
-        pool.initialize();
-    }
-
     /* (non-Javadoc)
      * @see com.googlecode.ehmem.performance.jsr107.CacheAdapter#get(java.lang.Object)
      */
